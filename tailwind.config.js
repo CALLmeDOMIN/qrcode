07077306528fa3545f825833a5d6e78d1cea67d1
Dominik Sieroń <71605737+CALLmeDOMIN@users.noreply.github.com>
2023-03-19 21:28:46 +0100

    final commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        'lG': '#d5e1ef',
+        'gB': '#858992',
+        'dB': '#1e2d40',
+      },
+    },
+  },
+  plugins: [],
+}
